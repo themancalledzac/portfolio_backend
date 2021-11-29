86706b2d4ded8010c6f515a475304836ab1cf859
Zac Edens <edens.zac@gmail.com>
2021-11-29 13:38:20 -0800

    updated index model file to include all mongoose models

---
+++ src/models/index.js
@@ -0,0 +1,8 @@
+const Image = require("./Image");
+const Author = require("./Author");
+const Film = require("./Film");
+const Keyword = require("./Keyword");
+const Lens = require("./Lens");
+// adding more later
+
+module.exports = { Image, Author, Film, Keyword, Lens };
