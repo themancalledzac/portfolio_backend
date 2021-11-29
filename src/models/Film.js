d3bfa722e2336090c98629a2493441f42fa141ad
Zac Edens <edens.zac@gmail.com>
2021-11-29 13:38:57 -0800

    added film mongoose model

---
+++ src/models/Film.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const filmSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    default: "Portra 800",
+  },
+  iso: {
+    type: Number,
+    required: true,
+    default: 800,
+  },
+  brand: {
+      type: String,
+      default: "Kodak",
+  },
+});
+
+const Film = model("Film", filmSchema);
+
+module.exports = Film;
