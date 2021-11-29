aae860949da8a7552c9262aa88b1bb923d221548
Zac Edens <edens.zac@gmail.com>
2021-11-29 12:11:33 -0800

    Initialized our schema page, lots of TODOs to do.

---
+++ src/schema.js
@@ -0,0 +1,81 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+    #Schema definitions go here
+    type Query {
+        "Get our homepage images"
+        imagesForHomePage: [Image!]!
+        "TODO: get images query for landscape page"
+        "TODO: get images query for portrait page"
+        "TODO: get images query for street page"
+        "TODO: get images query for event page"
+    }
+
+    type Mutation {
+        "TODO: Mutation for creating Keyword"
+        "TODO: Mutation for uploading image"
+        "TODO: Mutation for creating image"
+        "TODO: Mutation for deleting image"
+        "TODO: Mutation for creating author"
+        "TODO: Mutation for deleting author(?)"
+    }
+
+    type Image {
+        id: ID!
+        "Title of image"
+        title: String!
+        "Who is our author? Initially only I will be the author, but perhaps someday we can have guest images, or inspiration albums"
+        author: Author!
+        "Date taken"
+        date: String
+        "aperture of the image"
+        aperture: String
+        "exposure time of the image"
+        exposure: String
+        "focal length of the image"
+        focalLength: Int
+        "ISO of the image"
+        isoSpeed: String
+        "Lens used to create image"
+        lens: String
+        "if using film, we associate what film being used"
+        film: [Film]
+        "True if horizontal, false if vertical image"
+        horizontal: Boolean
+        "Google maps location of where the image was shot"
+        googleMapsLink: String
+        "The image's url, hosted on cloudinary"
+        imageUrl: String!
+        "The image's keywords for search functionality"
+        keywords: [Keyword!]!
+
+    }
+
+    type Author {
+        id: ID!
+        "Author's first and last name"
+        name: String!
+        "Author's profile picture url"
+        photo: String
+        "Author's Instagram"
+        instagram: String
+    }
+
+    type Keyword {
+        id: ID!
+        "Title of keyword"
+        title: String!
+    }
+    
+    type Film {
+        id: ID!
+        "Name of film being used"
+        title: String!
+        "box speed iso of film"
+        iso: String!
+        "brand name of film"
+        brand: String
+    }
+`;
+
+module.exports = typeDefs;
