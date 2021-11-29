dd5a17599362afa396d896d3ea9dd6ac26ce7e87
Zac Edens <edens.zac@gmail.com>
2021-11-29 13:38:39 -0800

    Added author mongoose model

---
+++ src/models/Author.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const authorSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    default: "Zechariah Edens",
+  },
+  photo: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/themancalledzac/image/upload/v1638220918/Portfolio/personal_portrait_color_oj6wxn.jpg",
+  },
+  instagram: {
+    type: String,
+    default: "https://www.instagram.com/themancalledzac/",
+  },
+});
+
+const Author = model("Author", authorSchema);
+
+module.exports = Author;
