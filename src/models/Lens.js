b3b62f5fae138b130d8fefc728e1a9c684564107
Zac Edens <edens.zac@gmail.com>
2021-11-29 13:39:41 -0800

    added Lens mongoose model

---
+++ src/models/Lens.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const lensSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    default: "NIKKOR Z 24-70mm f/4 S",
+  },
+  aperture: {
+    type: String,
+    required: true,
+    default: "f4",
+  },
+  fixedLens: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  focalLength: {
+    type: String,
+    required: true,
+    default: "24mm",
+  },
+  brand: {
+    type: String,
+    required: true,
+    default: "Nikon",
+  },
+});
+
+const Lens = model("Lens", lensSchema);
+
+module.exports = Lens;
