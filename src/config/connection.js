2335dffac069d8b86f636b8f79fb723d8204ea66
Zac Edens <edens.zac@gmail.com>
2021-11-29 16:32:08 -0800

    updated our mongoose connection to remove variables

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/portfolio_backend",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
+  process.env.MONGODB_URI || "mongodb://localhost:27017/portfolio_backend"
 );
 
 module.exports = mongoose.connection;
