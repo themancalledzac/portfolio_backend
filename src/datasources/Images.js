fbefd810dccb8c4071d8ba3e47e9a46fb8601cee
Zac Edens <edens.zac@gmail.com>
2021-12-08 16:08:27 -0800

    added an Images datasource

---
+++ src/datasources/Images.js
@@ -0,0 +1,17 @@
+//github.com/GraphQLGuide/apollo-datasource-mongodb/#findonebyid
+// https://www.apollographql.com/docs/apollo-server/data/data-sources/
+
+const { MongoDataSource } = require("apollo-datasource-mongodb");
+
+class Images extends MongoDataSource {
+  async getImage(imageId) {
+    const image = await this.findOneById(imageId);
+    return image;
+  }
+  async getAuthor(authorId) {
+    const author = await this.findOneById(authorId);
+    return author;
+  }
+}
+
+module.exports = Images;
